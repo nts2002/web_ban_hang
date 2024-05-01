d83a51397652252bcd79378eed26916b625346b4
nts2002 <kienminsk36@gmail.com>
2024-05-01 18:38:49 +0700

    update ckeditor, ckfinder

---
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        ApplicationDbContext dbConect = new ApplicationDbContext();
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var items = dbConect.News.OrderByDescending(x => x.Title).ToList();
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
--- WebBanHangOnline/Models/EF/News.cs
+++ WebBanHangOnline/Models/EF/News.cs
@@ -13,7 +13,7 @@ namespace WebBanHangOnline.Models.EF
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Bạn không để trống tiêu đề tin")]
         [StringLength(150)]
         public string Title { get; set; }
         public string Alias { get; set; }
