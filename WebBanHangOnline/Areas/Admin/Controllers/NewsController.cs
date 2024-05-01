4528df65c444f351f6662bc24dcabfefcc4412fa
nts2002 <kienminsk36@gmail.com>
2024-05-01 21:58:54 +0700

    crud for news

--- WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
 
 namespace WebBanHangOnline.Areas.Admin.Controllers
 {
@@ -20,6 +21,65 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         public ActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                model.CategoryId = 2;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                dbConect.News.Add(model);
+                dbConect.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = dbConect.News.Find(id);
+            if (item != null)
+            {
+                dbConect.News.Remove(item);
+                dbConect.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = dbConect.News.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                dbConect.News.Attach(model);
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                dbConect.Entry(model).Property(x => x.Title).IsModified = true;
+                //dbConect.Entry(model).Property(x => x.CategoryId).IsModified = true;
+                dbConect.Entry(model).Property(x => x.Description).IsModified = true;
+                dbConect.Entry(model).Property(x => x.Alias).IsModified = true;
+                dbConect.Entry(model).Property(x => x.SeoDescription).IsModified = true;
+                dbConect.Entry(model).Property(x => x.SeoKeyword).IsModified = true;
+                dbConect.Entry(model).Property(x => x.SeoTitle).IsModified = true;
+                dbConect.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+                dbConect.Entry(model).Property(x => x.ModeifiedBy).IsModified = true;
+                dbConect.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
--- WebBanHangOnline/Models/EF/News.cs
+++ WebBanHangOnline/Models/EF/News.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebBanHangOnline.Models.EF
 {
@@ -18,7 +19,8 @@ namespace WebBanHangOnline.Models.EF
         public string Title { get; set; }
         public string Alias { get; set; }
         public int CategoryId { get; set; } 
-        public string Description { get; set; } 
+        public string Description { get; set; }
+        [AllowHtml]
         public string Detail { get; set; } 
         public string Image { get; set; } 
         public string SeoDescription { get; set; } 
